e7c52b68c6df1396f0c0b6bcd8c4aec0c20f1bdc
Lix300 <95862129+Lix300@users.noreply.github.com>
2023-04-05 11:08:42 +0200

    verbesserung des Crud

--- app/Controllers/CRUD.php
+++ app/Controllers/CRUD.php
@@ -8,62 +8,69 @@ class Tasks extends BaseController {
     public function index() {
         return view('welcome_message');
     }
-// Ladet eine aufgabe anhand der id und gibt es an eine view weiter
-    public function show($id) {
-        $model = new example();
-        $data['setting'] = $model->getAufgaben($id);
-        return view('in die entsprechende view leiten', $data);
+
+    protected $request;
+
+    public function __construct()
+    {
+        $this->request = service('request');
     }
 
-//ladet alle aufgaben und gibt sie an eine view weiter
-    public function showAll() {
-    $model = new example();
-        $data['setting'] = $model->getAllAufgaben();
-        return view('in die entsprechende view leiten', $data);
-    }
+    public function generateCrud($table, $action)
+    {
+        $config = include('tables.php');
+        $model = new $config[$table]['model']();
+        $entity = new $config[$table]['entity']();
 
-    //leitet zur view weiter wo das formula ist für die erstellung von aufgaben
-    public function create() {
-        return view('in die entsprechende view leiten');
-    }
-
-    //speichert neue Aufgabe in DB
-    public function save(RequestInterface $request) {
-        $model = new example();
-        $aufgabe = new \App\Entities\Example($request->getPost());
-        if (!$model->createAufgaben($aufgabe)) {
-            return redirect()->back()->withInput()->with('errors', $model->errors());
+        switch ($action) {
+            case 'create':
+                $data = $this->request->getPost($config[$table]['fields']);
+                $entity->fill($data);
+                if (!$model->save($entity)) {
+                       // do error shit
+                }
+                // ausgabe an view das es erfolgreich gsi isch
+                break;
+            case 'read':
+                $data['rows'] = $model->findAll();
+              // date an di view schicke
+                break;
+            case 'update':
+                $data = $this->request->getPost($config[$table]['fields']);
+                $entity->fill($data);
+                if (!$model->save($entity)) {
+                       // do error shit
+                }
+                // ausgabe an view das es erfolgreich gsi isch
+                break;
+            case 'delete':
+                $model->delete($this->request->getPost('id'));
+                // ausgabe an view das es erfolgreich gsi isch
+                break;
+            default:
+                throw new InvalidArgumentException('Invalid action');
         }
-        return redirect()->to('/Example')->with('success', 'aufgabe created successfully!');
-
     }
 
-    // leitet an view weiter wo die daten bearbeitet werden können
-    public function edit($id) {
-        $model = new example();
-        $data['setting'] = $model->getAufgaben($id);
-        return view('in die entsprechende view leiten', $data);
-    }
-//akutallisiert die bearbeiteten daten in DB
-    public function update(RequestInterface $request, $id) {
-        $model = new example();
-        $aufgabe = new \App\Entities\Example($request->getPost());
-        $aufgabe->id = $id;
-        if (!$model->updateAufgaben($aufgabe)) { 
-            return redirect()->back()->withInput()->with('errors', $model->errors());
-        }
-        return redirect()->to('/Example')->with('success', 'aufgabe updated successfully!');
-    }
-//löscht alle daten aus der db einer besimmten Aufgabe
-    public function delete($id) {
-        $model = new example();
-        if (!$model->deleteAufgaben($id)) {
-            return redirect()->back()->withInput()->with('errors', $model->errors());
-        }
-        return redirect()->to('/Example')->with('success', 'aufgabe deleted successfully!');
+    public function create($table)
+    {
+        $this->generateCrud($table, 'create');
     }
 
+    public function read($table)
+    {
+        $this->generateCrud($table, 'read');
+    }
 
+    public function update($table)
+    {
+        $this->generateCrud($table, 'update');
+    }
+
+    public function delete($table)
+    {
+        $this->generateCrud($table, 'delete');
+    } 
 
     private function responseModel($model_name){
 
---
+++ app/Models/TaskModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Example extends Model {
+    protected $table = 'tasks';
+    protected $primaryKey = 'id';
+
+    protected $returnType = 'App\Entities\Tasks';
+
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = [
+        'id',
+        'title',
+        'description',
+        'due_date',
+        'completed'
+    ];
+
+    protected $useTimestamps = false;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = '';
+
+    protected $validationRules = [
+        'id' => 'require',
+        'title' => 'require',
+        'description' => 'require',
+        'due_date' => 'require',
+        'completed' => 'require'
+    ];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+
+    /**
+     * @param false $user_id
+     * @param false $status
+     * @return array|false
+     */
+
+    public function getSettingsByUserId($user_id = false) {
+        if ($user_id === false) {
+            return false;
+        }
+
+        return $this->asObject('App\Entities\Settings')->where(['id' => $user_id])->first();
+    }
+
+}
+
